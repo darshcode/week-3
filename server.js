54505fcdb535d47bfb6e76a4af739a3fd5111f78
darshcode <darsh.dhillon@live.com>
2022-10-05 01:31:25 -0400

    dot dot

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 // import third-party nodejs module Connect
 import express from "express";
+import cookieParser from "cookie-parser";
+import logger from 'morgan'
+import session from "express-session";
+
+//ES modules fix for __dirname 
+import path, {dirname} from 'path'
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 
 // instantiate app-server
 const app = express();
+
+// setup view engine ejs
+app.set('views', path.join(__dirname, '/views'));
+//while engine
+app.set('view engine', ejs);
+
+//thired party library
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false})); //fail safe for different devices.
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use.session({
+    secret:'MySecret',
+    saveUninitialized:false,
+    resave: false
+});
+
+
+
 
 // custom middleware
 function helloWorld(req, res, next){
